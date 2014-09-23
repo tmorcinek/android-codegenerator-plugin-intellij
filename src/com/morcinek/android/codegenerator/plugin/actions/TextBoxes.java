13587ae7b6405627338ee59fb65ff790d91d040b
Tomasz Morcinek <tomasz.morcinek@gmail.com>
2014-09-23 20:35:39 +0200

    Work in progress on Layout actions.

--- src/com/morcinek/android/codegenerator/plugin/actions/TextBoxes.java
+++ src/com/morcinek/android/codegenerator/plugin/actions/TextBoxes.java
@@ -1,10 +1,9 @@
 package com.morcinek.android.codegenerator.plugin.actions;
 
-import com.intellij.openapi.actionSystem.AnAction;
-import com.intellij.openapi.actionSystem.AnActionEvent;
-import com.intellij.openapi.actionSystem.PlatformDataKeys;
+import com.intellij.openapi.actionSystem.*;
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.ui.Messages;
+import com.intellij.openapi.vfs.VirtualFile;
 
 /**
  * Copyright 2014 Tomasz Morcinek. All rights reserved.
@@ -21,9 +20,24 @@ public class TextBoxes extends AnAction {
         // super("Text _Boxes","Item description",IconLoader.getIcon("/Mypackage/icon.png"));
     }
 
+    @Override
     public void actionPerformed(AnActionEvent event) {
         Project project = event.getData(PlatformDataKeys.PROJECT);
         String txt = Messages.showInputDialog(project, "What is your name?", "Input your name", Messages.getQuestionIcon());
         Messages.showMessageDialog(project, "Hello, " + txt + "!\n I am glad to see you.", "Information", Messages.getInformationIcon());
+    }
+
+    @Override
+    public void update(AnActionEvent e) {
+        e.getPresentation().setVisible(isVisible(e.getDataContext()));
+    }
+
+    private boolean isVisible(DataContext dataContext) {
+        VirtualFile data = CommonDataKeys.VIRTUAL_FILE.getData(dataContext);
+        return data != null && !data.isDirectory() && isInLayoutFolder(data);
+    }
+
+    private boolean isInLayoutFolder(VirtualFile data) {
+        return data.getParent().getPath().endsWith("layout");
     }
 }
