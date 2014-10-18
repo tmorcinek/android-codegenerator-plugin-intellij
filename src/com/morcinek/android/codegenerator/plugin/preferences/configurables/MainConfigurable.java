b7aaf01e2086563fcf6784b4855be3443382166f
Tomasz Morcinek <tomasz.morcinek@gmail.com>
2014-10-18 23:22:29 +0200

    Adding ApplicationConfigurable with children.
    Conflicts:
    	META-INF/plugin.xml

---
+++ src/com/morcinek/android/codegenerator/plugin/preferences/configurables/MainConfigurable.java
@@ -0,0 +1,68 @@
+package com.morcinek.android.codegenerator.plugin.preferences.configurables;
+
+import com.intellij.openapi.options.Configurable;
+import com.intellij.openapi.options.ConfigurationException;
+import org.jetbrains.annotations.Nls;
+import org.jetbrains.annotations.NonNls;
+import org.jetbrains.annotations.Nullable;
+
+import javax.swing.*;
+import java.awt.*;
+
+/**
+ * Copyright 2014 Tomasz Morcinek. All rights reserved.
+ */
+public class MainConfigurable implements Configurable.Composite, Configurable.NoScroll, Configurable {
+
+    private Configurable[] configurables;
+
+    public MainConfigurable() {
+        super();
+    }
+
+    @Nls
+    @Override
+    public String getDisplayName() {
+        return "Android Code Generator";
+    }
+
+    @Nullable
+    @NonNls
+    @Override
+    public String getHelpTopic() {
+        return null;
+    }
+
+    @Override
+    public JComponent createComponent() {
+        System.out.println("CreateComponent");
+        JPanel panel = new JPanel();
+        JLabel label = new JLabel("<html>Here you can edit 'Android Code Generator Plugin' settings. In children pages you can edit template for each code generation method.</html>");
+        label.setVerticalAlignment(SwingConstants.TOP);
+        panel.add(label, BorderLayout.PAGE_START);
+        return panel;
+    }
+
+    public void disposeUIResources() {
+    }
+
+    @Override
+    public boolean isModified() {
+        return false;
+    }
+
+    @Override
+    public void apply() throws ConfigurationException {
+    }
+
+    @Override
+    public void reset() {
+    }
+
+    public Configurable[] getConfigurables() {
+        if (configurables == null) {
+            configurables = new Configurable[]{new TemplateConfigurable()};
+        }
+        return configurables;
+    }
+}
