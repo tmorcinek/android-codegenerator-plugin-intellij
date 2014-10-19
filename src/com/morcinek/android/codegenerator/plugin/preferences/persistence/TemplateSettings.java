025eee1734b295a71d7bff7e30f5b444ba317581
Tomasz Morcinek <tomasz.morcinek@gmail.com>
2014-10-19 21:52:05 +0200

    Adding TemplateSettings.

---
+++ src/com/morcinek/android/codegenerator/plugin/preferences/persistence/TemplateSettings.java
@@ -0,0 +1,61 @@
+package com.morcinek.android.codegenerator.plugin.preferences.persistence;
+
+import com.google.common.collect.Maps;
+import com.intellij.openapi.components.*;
+import com.intellij.util.xmlb.XmlSerializerUtil;
+import com.morcinek.android.codegenerator.codegeneration.templates.TemplatesProvider;
+import com.morcinek.android.codegenerator.plugin.codegenerator.CodeGeneratorFactory;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.Map;
+
+/**
+ * Copyright 2014 Tomasz Morcinek. All rights reserved.
+ */
+@State(
+        name = "CodeGeneratorTemplateSettings",
+        storages = {
+                @Storage(file = StoragePathMacros.APP_CONFIG + "/code_generator_template_settings.xml")
+        }
+)
+public class TemplateSettings implements PersistentStateComponent<TemplateSettings>, TemplatesProvider {
+
+    private Map<String, String> templateValues = Maps.newHashMap();
+
+    private TemplatesProvider templatesProvider = new CodeGeneratorFactory.ResourceTemplateProvider();
+
+    public static TemplateSettings getInstance() {
+        return ServiceManager.getService(TemplateSettings.class);
+    }
+
+    public Map<String, String> getTemplateValues() {
+        return templateValues;
+    }
+
+    public void setTemplateValues(Map<String, String> templateValues) {
+        this.templateValues = templateValues;
+    }
+
+    @Nullable
+    @Override
+    public TemplateSettings getState() {
+        return this;
+    }
+
+    @Override
+    public void loadState(TemplateSettings templateSettings) {
+        XmlSerializerUtil.copyBean(templateSettings, this);
+    }
+
+    @Override
+    public String provideTemplateForName(String templateName) {
+        if (templateValues.containsKey(templateName)) {
+            return templateValues.get(templateName);
+        }
+        return templatesProvider.provideTemplateForName(templateName);
+    }
+
+    public String setTemplateForName(String templateName, String template) {
+        return templateValues.put(templateName, template);
+    }
+}
